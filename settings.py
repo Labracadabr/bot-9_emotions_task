53521ef91d64a3d1223b26c22e3c40dab51eeaf3
Labracadabr <124514026+Labracadabr@users.noreply.github.com>
2023-09-05 17:22:49 +0300

    v1 extr ref

--- settings.py
+++ settings.py
@@ -4,13 +4,14 @@ mngr: str = '@its_dmitrii'
 # список id админов. валидация приходит только первому по списку
 # admins: list[str] = ["992863889"]   # Дима
 admins: list[str] = ["899038082", "992863889"]   # Илья, Дима
+# 2137731767 Лаврук Кристина
 
 # База данных со статусами заданий юзеров. тк я не умею в бд, то это просто json
 baza_task = 'user_status.json'
 baza_info = 'user_info.json'
 
 # каналы сбора
-referrals = ('smeight', 'gulnara', 'its_dmitrii', 'Natali', 'TD', 'Marina')
+referrals = ('smeight', 'gulnara', 'its_dmitrii', 'Natali', 'TD', 'Marina', 'airplane', 'one_more')
 
 # # игнорить ли сообщения, присланные во время отключения бота
 # ignor: bool = False

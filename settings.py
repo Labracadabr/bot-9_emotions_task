530046039e15cc40b1e425b3796c8159aacb32bb
Labracadabr <124514026+Labracadabr@users.noreply.github.com>
2023-09-06 12:13:53 +0300

    v1 мелочь2

--- settings.py
+++ settings.py
@@ -9,6 +9,7 @@ admins: list[str] = ["899038082", "992863889"]   # Илья, Дима
 # База данных со статусами заданий юзеров. тк я не умею в бд, то это просто json
 baza_task = 'user_status.json'
 baza_info = 'user_info.json'
+logs = 'logs.json'
 
 # каналы сбора
 referrals = ('smeight', 'gulnara', 'its_dmitrii', 'Natali', 'TD', 'Marina', 'airplane', 'one_more')

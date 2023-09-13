1abfb4399ceae43300bca8cc41dcccd437e48875
Labracadabr <124514026+Labracadabr@users.noreply.github.com>
2023-09-13 13:24:26 +0300

    v1 хотфикс валид юзеров без пд

--- handler_admin.py
+++ handler_admin.py
@@ -1,7 +1,7 @@
 from aiogram import Router, Bot, F
 from aiogram.types import Message, CallbackQuery, FSInputFile
 from settings import admins, baza_task, baza_info, logs
-from bot_logic import Access, log, id_from_text, FSM
+from bot_logic import Access, log, id_from_text, FSM, get_tsv, accept_user
 from lexic import lex
 import json
 import os
@@ -39,39 +39,16 @@ async def admin_ok(callback: CallbackQuery, bot:Bot):
 
     # worker = вытащить id из текста сообщения
     worker = id_from_text(msg.text)
+
+    # принять все файлы
+    await accept_user(TKN, bot, worker)
     log(logs, worker, 'admin_accept')
-
-    # проставить accept во всех файлах и записать ссылки для скачивания
-    with open(baza_task, 'r', encoding='utf-8') as f:
-        data = json.load(f)
-    urls = []
-    tasks = data[worker]
-    for file in tasks:
-        tasks[file][0] = 'accept'
-        print(tasks[file][0])
-
-        # добыть ссылку по file_id
-        try:
-            file_info = await bot.get_file(tasks[file][1])
-            file_url = file_info.file_path
-            url = f'https://api.telegram.org/file/bot{TKN}/{file_url}'
-        except TelegramBadRequest:
-            url = 'unavailable'
-            print('file unavailable')
-        urls.append(url)
-
-    # сохранить статусы заданий
-    data.setdefault(worker, tasks)
-    with open(baza_task, 'w', encoding='utf-8') as f:
-        json.dump(data, f, indent=2, ensure_ascii=False)
 
     # убрать кнопки админа
     await bot.edit_message_text(f'{msg.text}\n✅ Принято', msg.chat.id, msg.message_id, reply_markup=None)
     # Дать юзеру аппрув
     await bot.send_message(chat_id=worker, text=lex['all_approved']+f'id{worker}')
     log(logs, worker, 'admin_accept')
-
-
     # # сохранить ссылки
     # gc = pygsheets.authorize(service_file='token.json')
     # sheet_url = 'https://docs.google.com/spreadsheets/d/1dlZdboea3OAzNpivRxgDiQ6SaW14RjHdfFD-77HwGiQ/edit#gid=0'
@@ -82,28 +59,8 @@ async def admin_ok(callback: CallbackQuery, bot:Bot):
     #     pass
 
     # сохранить ссылки в g_sheet, в отдельный tsv и print в консоль
-    folder = 'accepted_files'
-    if not os.path.exists(folder):
-        os.makedirs(folder)
-    path = f'{folder}/accepted_{worker}.tsv'
-    with open(path, 'w', encoding='UTF-8') as file:
-        tasks_dict = lex['tasks']
 
-        # первая строка таблицы
-        row = ['accept_time:', f'{str(msg.date.date())}', f'{msg.date.time()}']
-        print('\t'.join(row), file=file)
-        # spreadsheet.worksheet_by_title(title=worker).append_table(values=row)
-
-        #  остальные строки
-        for i, file_num in enumerate(tasks_dict):
-            try:
-                row = [file_num, tasks_dict[file_num].split()[3], urls[i]]
-            except IndexError:
-                break
-            print('\t'.join(tuple(map(str, row))), file=file)
-            print(row)
-            # spreadsheet.worksheet_by_title(worker).append_table(values=row)
-
+    path = await get_tsv(TKN, bot, msg, worker)
     # отправить tsv админу
     for i in admins:
         await bot.send_document(chat_id=i, document=FSInputFile(path=path))
@@ -163,21 +120,24 @@ async def reply_to_msg(msg: Message, bot: Bot):
                 data_inf = json.load(f)
             # if worker not in data_inf:
             #     print(worker, 'new user from:', None)
-            #     data_tsk.setdefault(worker, lex['user_account'])
-            #
-            #     # создать запись ПД
-            #     print(user_id, 'pd created')
-            #     info = lex['user_pd']
-            #     info['referral'] = None
-            #     info['first_start'] = None
-            #     info['tg_username'] = message.from_user.username
-            #     info['tg_fullname'] = message.from_user.full_name
-            #     print(info)
-            #     data_inf.setdefault(worker, info)
+                # data_tsk.setdefault(worker, lex['user_account'])
+                #
+                # # создать запись ПД
+                # print(user_id, 'pd created')
+                # info = lex['user_pd']
+                # info['referral'] = None
+                # info['first_start'] = None
+                # info['tg_username'] = message.from_user.username
+                # info['tg_fullname'] = message.from_user.full_name
+                # print(info)
+                # data_inf.setdefault(worker, info)
 
-            ref = data_inf[worker]['referral']
-            username = data_inf[worker]['tg_username']
-            fullname = data_inf[worker]['tg_fullname']
+            if worker in data_inf:
+                ref = data_inf[worker]['referral']
+                username = data_inf[worker]['tg_username']
+                fullname = data_inf[worker]['tg_fullname']
+            else:
+                ref = username = fullname = '?'
 
             # обновить сообщение у админа и дописать причину отказа
             await bot.edit_message_text(f'❌ Отклонено. Причина:\n{admin_response}', orig.chat.id, orig.message_id,
@@ -303,51 +263,26 @@ async def adm_msg(msg: Message, bot: Bot):
         await bot.send_document(chat_id=user, document=FSInputFile(path=baza_info))
         await bot.send_document(chat_id=user, document=FSInputFile(path=logs))
 
+    # принять все файлы по айди юзера
+    elif txt.lower().startswith('accept id'):
+        # worker = вытащить id из текста сообщения
+        worker = id_from_text(txt)
+
+        # принять все файлы
+        await accept_user(TKN, bot, worker)
+        log(logs, worker, 'admin_accept')
+        await msg.answer(f'✅ Приняты файлы от id{worker}')
+
     # отпр тсв со всем что юзер скинул на данный момент
     elif txt.lower().startswith('send id'):
         # worker = вытащить id из текста сообщения
         worker = id_from_text(txt)
 
-        #  чтение БД
-        with open(baza_task, 'r') as f:
-            data = json.load(f)
-
-        urls = []
-        tasks = data[worker]
-        for file in tasks:
-            # добыть ссылку по file_id
-            try:
-                file_info = await bot.get_file(tasks[file][1])
-                file_url = file_info.file_path
-                url = f'https://api.telegram.org/file/bot{TKN}/{file_url}'
-            except TelegramBadRequest:
-                url = 'unavailable'
-                print('file unavailable')
-
-            urls.append(url)
-
-        folder = 'sent_files'
-        if not os.path.exists(folder):
-            os.makedirs(folder)
-        path = f'{folder}/sent_{worker}.tsv'
-        with open(path, 'w', encoding='UTF-8') as file:
-            tasks_dict = lex['tasks']
-
-            # первая строка таблицы
-            row = ['create_time:', f'{str(msg.date.date())}', f'{msg.date.time()}']
-            print('\t'.join(row), file=file)
-
-            #  остальные строки
-            for i, file_num in enumerate(tasks_dict):
-                try:
-                    row = [file_num, tasks_dict[file_num].split()[3], urls[i]]
-                except IndexError:
-                    break
-                print('\t'.join(tuple(map(str, row))), file=file)
-                print(row)
-
         # отправить tsv админу
+        path = await get_tsv(TKN, bot, msg, worker)
         await bot.send_document(chat_id=msg.from_user.id, document=FSInputFile(path=path))
+        log(logs, worker, 'adm get_tsv')
+
     else:
         await msg.answer('Куда ты пишешь? Ответь на сообщение с крестиком')
 

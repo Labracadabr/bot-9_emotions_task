4fb1df37012684126afd4902ed4c34ddd93d5171
Labracadabr <124514026+Labracadabr@users.noreply.github.com>
2023-09-20 18:36:58 +0300

    v1 лиза

--- settings.py
+++ settings.py
@@ -1,15 +1,14 @@
 dima = "992863889"
 ilya = "899038082"
 kris = "2137731767"
+liza = '677214436'
 
-# Список id админов. Файлы приходит только первому по списку
+# Список id админов. Файлы приходят только первому по списку
 admins: list[str] = [ilya, dima]
 
-# Список id валидаторов. Можно вписать либо одного, либо двух - тогда одному будут идти четные, второму нечет
+# Список id валидаторов. Можно вписать либо ноль, либо одного, либо двух - тогда одному будут идти четные, второму нечет
 # Им приходят файлы и кнопки, доступны команды валидации
-validators: list[str] = [kris]
-
-# admins: list[str] = ["2137731767", "899038082", "992863889"]
+validators: list[str] = [liza]
 
 
 # где хранятся данные. тк я не умею в бд, то это просто json
@@ -19,7 +18,8 @@ logs = 'logs.json'
 tasks_tsv = 'tasks.tsv'
 
 # каналы сбора
-referrals = ('smeight', 'gulnara', 'its_dmitrii', 'Natali', 'TD', 'Marina', 'airplane', 'one_more')
+referrals = ('smeight', 'gulnara', 'its_dmitrii', 'Natali', 'TD', 'Marina', 'schura', 'cat', 'airplane', 'one_more')
+# https://t.me/TdTasksBot?start=...
 
 # # игнорить ли сообщения, присланные во время отключения бота
 # ignor: bool = False

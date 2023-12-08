749ff88cd7133f4f8ee9af6a7070c1739a37c02d
Dmitrii <dminokin@gmail.com>
2023-12-08 12:12:34 +0300

    названия, вопрос страны

--- handler_pd.py
+++ handler_pd.py
@@ -1,16 +1,16 @@
 import json
 from aiogram import Router, Bot, F
 from aiogram.filters import Command, CommandStart, StateFilter, CommandObject
-from bot_logic import log, Access, FSM # dwnld_photo_or_doc
+from bot_logic import log, FSM
 from config import Config, load_config
-from settings import admins, baza_task, baza_info, referrals, logs
+from settings import baza_info, logs
 from lexic import lex
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.storage.memory import MemoryStorage
-from aiogram.types import CallbackQuery, Message
+from aiogram.types import Message
 
 
-# Инициализация всяких ботских штук
+# Инициализация
 router: Router = Router()
 config: Config = load_config()
 TKN: str = config.tg_bot.token
@@ -19,10 +19,9 @@ storage: MemoryStorage = MemoryStorage()
 
 # команда /personal - заполнить ПД
 @router.message(Command(commands=['personal']))
-async def cancel_command(msg: Message, state: FSMContext):
+async def personal_command(msg: Message, state: FSMContext):
     user = str(msg.from_user.id)
-    print(user, msg.text)
-    log(logs, user, msg.text)
+    await log(logs, user, msg.text)
     # чтение БД
     with open(baza_info, 'r', encoding='utf-8') as f:
         data_inf = json.load(f)
@@ -32,8 +31,7 @@ async def cancel_command(msg: Message, state: FSMContext):
         data_inf[user]
     except KeyError:
         # создать запись ПД
-        print(user, 'pd created')
-        log(logs, user, 'pd created')
+        await log(logs, user, 'pd created')
         info = lex['user_pd']
         info['first_start'] = msg.date.strftime("%d/%m/%Y %H:%M")
         info['tg_username'] = msg.from_user.username
@@ -46,31 +44,25 @@ async def cancel_command(msg: Message, state: FSMContext):
     # проверить заполнены ли уже перс данные
     try:
         data_inf[user]['gender']
-        log(logs, user, 'try')
-        print('try')
     except KeyError:
         data_inf[user].setdefault('gender', None)
-        log(logs, user, 'exc')
-        print('exc')
     if not data_inf[user]['gender']:
         # спросить возраст
         await state.set_state(FSM.age)
         await msg.answer(lex['age'])
-        print(user, 'ask age')
-        log('logs.json', user, 'ask age')
+        await log(logs, user, 'ask age')
 
     else:
         await msg.answer(lex['pd_ok'])
-        log('logs.json', user, 'pd ok')
+        await log(logs, user, 'pd already ok')
 
 
 # юзер отправляет возраст
 @router.message(F.content_type.in_({'text'}), StateFilter(FSM.age))
-async def cancel(msg: Message, bot: Bot, state: FSMContext):
+async def personal_age(msg: Message, state: FSMContext):
     user = str(msg.from_user.id)
     age = msg.text
-    print(user, age)
-    log('logs.json', user, age)
+    await log(logs, user, f'age: {age}')
 
     # проверка правильного ввода
     if age.isnumeric() and len(age) == 2:
@@ -86,7 +78,7 @@ async def cancel(msg: Message, bot: Bot, state: FSMContext):
         await state.set_state(FSM.gender)
         await msg.answer(text=lex['gender'])
         print(user, 'ask gender')
-        log('logs.json', user, 'ask gender')
+        await log(logs, user, 'ask gender')
 
     else:
         await msg.reply(text=lex['age_bad'])
@@ -94,14 +86,13 @@ async def cancel(msg: Message, bot: Bot, state: FSMContext):
 
 # юзер отправляет пол
 @router.message(F.content_type.in_({'text'}), StateFilter(FSM.gender))
-async def cancel(msg: Message, state: FSMContext):
+async def personal_sex(msg: Message, state: FSMContext):
     user = str(msg.from_user.id)
     gender = msg.text.lower()
-    print(user, gender)
-    log('logs.json', user, gender)
+    await log(logs, user, f'gender: {gender}')
 
     # проверка правильности ввода
-    if gender == 'm' or gender == 'f':
+    if gender in ('m', 'f'):
         # чтение БД
         with open(baza_info, 'r', encoding='utf-8') as f:
             data_inf = json.load(f)
@@ -111,42 +102,62 @@ async def cancel(msg: Message, state: FSMContext):
         with open(baza_info, 'w', encoding='utf-8') as f:
             json.dump(data_inf, f, indent=2, ensure_ascii=False)
 
-        if data_inf[user]['referral']:
-            # если асессор из TD, то спросить фио
-
-            if data_inf[user]['referral'].lower() == 'td':
-                await state.set_state(FSM.fio)
-                await msg.answer(text=lex['fio'])
-                log('logs.json', user, 'ask gender')
-                print(user, 'fio')
-
-            # если толокер, то дать id
-            elif data_inf[user]['referral'].lower() == 'toloka':
-                print(user, 'toloka')
-                await msg.answer(text=lex['tlk_ok'])
-                await msg.answer(text=f'<code>{user}</code>', parse_mode='HTML')
-                log('logs.json', user, 'tlk ok')
-                await state.clear()
-
-            else:
-                await msg.answer(text=lex['pd_ok'])
-                log('logs.json', user, 'gender ok')
-                await state.clear()
-        else:
-            await msg.answer(text=lex['pd_ok'])
-            log('logs.json', user, 'gender ok')
-            await state.clear()
+        # спросить страну
+        await msg.answer(text=lex['country'])
+        await log(logs, user, 'gender ok')
+        await state.set_state(FSM.country)
     else:
         await msg.reply(text=lex['gender_bad'])
 
 
-# юзер отправляет ФИО
+# юзер отправляет страну
+@router.message(F.content_type.in_({'text'}), StateFilter(FSM.country))
+async def personal_country(msg: Message, state: FSMContext):
+    user = str(msg.from_user.id)
+    country = msg.text.capitalize()
+    await log(logs, user, f'country: {country}')
+
+    # чтение БД
+    with open(baza_info, 'r', encoding='utf-8') as f:
+        data_inf = json.load(f)
+
+    # сохранить страну в БД
+    data_inf[user]['fio'] = country
+    with open(baza_info, 'w', encoding='utf-8') as f:
+        json.dump(data_inf, f, indent=2, ensure_ascii=False)
+
+    if data_inf[user]['referral']:
+        # если асессор из TD, то спросить фио
+        if data_inf[user]['referral'].lower() == 'td':
+            await state.set_state(FSM.fio)
+            await msg.answer(text=lex['fio'])
+            await log(logs, user, 'ask fio')
+
+        # если толокер, то дать id
+        elif data_inf[user]['referral'].lower() == 'toloka':
+            print(user, 'toloka')
+            await msg.answer(text=lex['tlk_ok'])
+            await msg.answer(text=f'<code>{user}</code>', parse_mode='HTML')
+            await log(logs, user, 'tlk ok')
+            await state.clear()
+
+        else:
+            await msg.answer(text=lex['pd_ok'])
+            await log(logs, user, 'country ok')
+            await state.clear()
+    else:
+        await msg.answer(text=lex['pd_ok'])
+        await log(logs, user, 'country ok')
+        await state.clear()
+
+
+# юзер отправляет фио
 @router.message(F.content_type.in_({'text'}), StateFilter(FSM.fio))
-async def cancel(msg: Message, state: FSMContext):
+async def personal_fio(msg: Message, state: FSMContext):
     user = str(msg.from_user.id)
     fio = msg.text
     print(user, fio)
-    log('logs.json', user, fio)
+    await log(logs, user, f'fio: {fio}')
     slov = len(fio.split())
 
     # проверка правильности ввода
@@ -161,7 +172,7 @@ async def cancel(msg: Message, state: FSMContext):
             json.dump(data_inf, f, indent=2, ensure_ascii=False)
 
         await msg.answer(text=lex['pd_ok'])
-        log('logs.json', user, 'fio ok')
+        await log(logs, user, 'fio ok')
         await state.clear()
     else:
         await msg.reply(text=lex['fio_bad'])
